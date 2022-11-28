e19c25fbffd8444df0dd3fc80e2b6b9a4c916ddf
Bolognesia <bele.ramirez@gmail.com>
2022-11-28 15:49:33 +1300

    28nov-1549

---
+++ js/date-picker.js
@@ -0,0 +1,15 @@
+$('#check-in-date').flatpickr({
+    minDate: "today",
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+    
+
+})
+
+$('#check-out-date').flatpickr({
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+}
+);
