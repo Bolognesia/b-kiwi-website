69779dfa0297d7fc70c715699ca20ff991b3a465
Bolognesia <bele.ramirez@gmail.com>
2022-12-02 11:56:39 +1300

    02dec-1156

--- js/search-landing-page.js
+++ js/search-landing-page.js
@@ -7,8 +7,7 @@ let accommodation_db =[
         acc_guest_number: '1-4 max.',
         acc_nights_number: '2-15 nights max.',
         acc_description: 'Relax in the beautiful cottage garden during the day and into the evening be captivated by the breathtaking night sky. Just 5 minutes walk to stunning Lake Tekapo and the Church of the Good Shepherd. The studio is spacious, warm and light and has a separate entrance and garden access. Super King size bed. An ideal space for couples. Not suitable for children or infants. ',
-        acc_price_per_night: '$240',
-        acc_price_total: '',
+        acc_price_per_night: 240,
         id: '1',
     },
 
@@ -19,8 +18,7 @@ let accommodation_db =[
         acc_guest_number: '1-2 max.',
         acc_nights_number: '1-5 nights max.',
         acc_description: 'Peppers Bluewater Resort is a 4.5-star hotel close to the shores of Lake Tekapo. This resort offers luxury accommodation, great views and is ideally positioned for exploring the Mackenzie Basin and Southern Alps. It is a 10-minute walk from the Church of the Good Shepherd. The air-conditioned accommodation has a private bathroom and tea and coffee making facilities. Most feature a balcony, fully equipped kitchen and mountain or lake views. WiFi access is available for an additional charge.',
-        acc_price_per_night: '$157',
-        acc_price_total: '',
+        acc_price_per_night: 157,
         id: '2',
     },
 
@@ -31,7 +29,7 @@ let accommodation_db =[
         acc_guest_number: '1 max.',
         acc_nights_number: '1-10 nights max.',
         acc_description: 'Offering a childrens playground, a guest lounge and budget accommodation options, Tailor Made Tekapo Accommodation - Guesthouse & Hostel is located in Lake Tekapo. Guests enjoy free on-site parking. Guests enjoy unlimited WiFi per day. Tailor Made Tekapo Accommodation - Guesthouse & Hostel is 5 minutes walk from the Cairns Golf Course. It is 5 minutes drive from Tekapo Springs and the Church of the Good Shepherd. The independent Tailor Made Tekapo Accommodation - Guesthouse & Hostel offers a garden, a shared kitchen and a ski storage room.',
-        acc_price_per_night: '$30',
+        acc_price_per_night: 30,
         acc_price_total: '',
         id: '3',
     },
@@ -43,8 +41,7 @@ let accommodation_db =[
         acc_guest_number: '2-4 max.',
         acc_nights_number: '3-10 nights max.',
         acc_description: 'Situated in the centre of the village, Lake Tekapo Village Motel offers rooms with lake views. Free WiFi and free private parking is available. Twizel is a 40-minute drive from the property. Guests can choose from Studios, Two-Bedroom Apartments and a Penthouse Suite that accommodates the top floor of the motel. All rooms at Lake Tekapo Village Motel feature a flat-screen TV, private bathroom with hairdryer, heating and ironing facilities.',
-        acc_price_per_night: '90',
-        acc_price_total: '',
+        acc_price_per_night: 90,
         id: '4',
     },
 ]
@@ -61,25 +58,23 @@ let accommodation_db =[
 // current_card.getElementsByClassName('accomodation-name')[0].innerHTML = accommodation_db[0].acc_name;
 
 function populate_acc_cards(accObj){
-    console.log(accObj);
+    // console.log(accObj);
     // console.log(accommodation_db[0].acc_type);
 console.log(accObj.acc_type);
 
     let current_card = document.getElementById(accObj.acc_type);
 
-    console.log(current_card);
+    // console.log(current_card);
 
-    console.log(     accObj.acc_name     );
+    // console.log(     accObj.acc_name     );
 
+    current_card.getElementsByClassName('card-img')[0].src = accObj.acc_image;
     current_card.getElementsByClassName('accomodation-name')[0].innerHTML = accObj.acc_name;
     current_card.getElementsByClassName('type-accommodation')[0].innerHTML = accObj.acc_type;
     current_card.getElementsByClassName('guest-number')[0].innerHTML = accObj.acc_guest_number;
-    // current_card.getElementsByClassName('acc-nights')[0].innerHTML = accObj.acc_nights_number;
+    current_card.getElementsByClassName('acc-nights')[0].innerHTML = accObj.acc_nights_number;
     current_card.getElementsByClassName('accommodation-description-paragraph')[0].innerHTML = accObj.acc_description;
-    current_card.getElementsByClassName('price')[0].innerHTML = accObj.acc_price_per_nigh;
-    
-    
-
+    current_card.getElementsByClassName('price')[0].innerHTML = '$' + accObj.acc_price_per_night;
 }
 
 for (let i = 0; i < accommodation_db.length; i++)
@@ -102,12 +97,12 @@ populate_acc_cards(accommodation_db[i]);
 $('#search-btn').click(function(){
         // console.log($('#check-in-date').val());
 
+        //DECLARE VARIABLES
         let check_in_value = $('#check-in-date').val();
         // console.log(check_in_value);
         let check_out_value = $('#check-out-date').val();
         let guests_input = $('#guests-input').val();
         // console.log(guests_input);
-
         let difference = calculate_day_difference(check_in_value,check_out_value);
         console.log(difference);
 
@@ -147,21 +142,40 @@ $('#search-btn').click(function(){
 
         if (guests_input >= 2 && guests_input <=4 && difference >= 3 && difference <= 10){
             $('#Motel').removeClass('no-display');
-            // console.log('test');
+            
         }
 
         else
             $('#Motel').addClass('no-display');
+
 
         //GET ITEM LOCAL STORAGE
         localStorage.setItem('guests_input',guests_input);
         localStorage.setItem('check_in_value', check_in_value);
         localStorage.setItem('check_out_value',check_out_value);
         
+
+        // //DECLARE VARIABLES FROM CARD ACCCOMODATION
+        // // let accommodation_name = document.getElementsByClassName('accomodation-name').innerHTML;
+
+        // for (i=0; i < accommodation_db.length; i++){
+        //     if()
+        //     let accommodation_name = document.getElementsByClassName('accomodation-name')[i].innerHTML;
+        //     console.log(accommodation_name);
+        //     localStorage.setItem('accomodation_name', accommodation_name);
+        // }
+        
         //REACHING SEARCH RESULTS PAGE WITH JS
         // window.location = './search-results.html';  
+
+        let total_price_displays = document.getElementsByClassName('total-price')
+        for(i=0; i < accommodation_db.length; i++){
+        total_price_displays[i].innerHTML = '$' + difference * accommodation_db[i].acc_price_per_night;
+        
+        }
     }
 
+    
     
 );
 
--- js/search-results.js
+++ js/search-results.js
@@ -1,3 +1,11 @@
-let received_check_in_value = localStorage.getItem('check_in_value');
-$('#check-in-date').html(received_check_in_value)
-console.log(received_check_in_value);
+// let received_check_in_value = localStorage.getItem('check_in_value');
+// $('#check-in-date').html(received_check_in_value)
+// console.log(received_check_in_value);
+
+function populate_cards_new_page(){
+        let new_card_acc_name = localStorage.getItem('accomodation_name');
+        console.log(new_card_acc_name);
+        document.getElementsByClassName('accomodation-name')[0].innerHTML = new_card_acc_name;
+
+        console.log(document.getElementsByClassName('accomodation-name')[0]);
+    }
